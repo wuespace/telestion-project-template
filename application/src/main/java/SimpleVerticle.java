ee1670b44dbbb47cd8954fe393df939c73c8b676
Ludwig Richter <ludwig.richter@wuespace.de>
2021-12-23 21:43:51 +0100

    feat: Fetch Telestion Core packages from Maven Central and update sample code to match current implementation

--- application/src/main/java/SimpleVerticle.java
+++ application/src/main/java/SimpleVerticle.java
@@ -1,17 +1,20 @@
-import io.vertx.core.AbstractVerticle;
+import de.wuespace.telestion.api.verticle.GenericConfiguration;
+import de.wuespace.telestion.api.verticle.TelestionVerticle;
+import de.wuespace.telestion.api.verticle.trait.WithEventBus;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 
-public class SimpleVerticle extends AbstractVerticle {
-  public static void main(String[] args) {
-    var vertx = Vertx.vertx();
-    vertx.deployVerticle(SimpleVerticle.class, new DeploymentOptions());
-  }
+public class SimpleVerticle extends TelestionVerticle<GenericConfiguration> implements WithEventBus {
+	public static void main(String[] args) {
+		var vertx = Vertx.vertx();
+		vertx.deployVerticle(SimpleVerticle.class, new DeploymentOptions());
+	}
 
-  @Override
-  public void start() {
-    vertx.setPeriodic(1000, id -> {
-      vertx.eventBus().publish("simple-verticle-out-address", "Hello World");
-    });
-  }
+	@Override
+	public void onStart() {
+		vertx.setPeriodic(1000, id -> {
+			logger.debug("Publish new message on event bus");
+			publish("simple-verticle-out-address", "Hello World");
+		});
+	}
 }
