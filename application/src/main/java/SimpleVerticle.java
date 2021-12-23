944021670ab0897837bd04f304538ce0cc2538ac
Ludwig Richter <ludwig.richter@wuespace.de>
2021-12-23 21:36:28 +0100

    fix: Remove package path from Java sample entirely and generate it on initialization

--- application/src/main/java/SimpleVerticle.java
+++ application/src/main/java/SimpleVerticle.java
@@ -1,8 +1,13 @@
-package ##GROUP_ID##;
-
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
 
 public class SimpleVerticle extends AbstractVerticle {
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.deployVerticle(SimpleVerticle.class, new DeploymentOptions());
+  }
+
   @Override
   public void start() {
     vertx.setPeriodic(1000, id -> {
