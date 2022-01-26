82daf45985ad041c88c03fe68a253a655d743af8
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-26 23:25:59 +0100

    feat(application): Add `WithTiming` trait to `SimpleVerticle`

--- application/src/main/java/SimpleVerticle.java
+++ application/src/main/java/SimpleVerticle.java
@@ -1,10 +1,13 @@
 import de.wuespace.telestion.api.verticle.GenericConfiguration;
 import de.wuespace.telestion.api.verticle.TelestionVerticle;
 import de.wuespace.telestion.api.verticle.trait.WithEventBus;
+import de.wuespace.telestion.api.verticle.trait.WithTiming;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 
-public class SimpleVerticle extends TelestionVerticle<GenericConfiguration> implements WithEventBus {
+import java.time.Duration;
+
+public class SimpleVerticle extends TelestionVerticle<GenericConfiguration> implements WithEventBus, WithTiming {
 	public static void main(String[] args) {
 		var vertx = Vertx.vertx();
 		vertx.deployVerticle(SimpleVerticle.class, new DeploymentOptions());
@@ -12,7 +15,7 @@ public class SimpleVerticle extends TelestionVerticle<GenericConfiguration> impl
 
 	@Override
 	public void onStart() {
-		vertx.setPeriodic(1000, id -> {
+		interval(Duration.ofSeconds(1), id -> {
 			logger.debug("Publish new message on event bus");
 			publish("simple-verticle-out-address", "Hello World");
 		});
